b71ac355232ff5104a8981cc2d7f918dc1cffc48
aayu-11 <ayushkumar.ak2409@gmail.com>
2024-09-03 21:28:05 +0530

    db connection

--- 04_MegaProjectYoutubeClone/src/index.js
+++ 04_MegaProjectYoutubeClone/src/index.js
@@ -1,1 +1,28 @@
-console.log("Hello World");
+import dotenv from "dotenv";
+
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "../.env",
+});
+console.log(process.env.PORT);
+console.log(process.env.MONGODB_URI);
+console.log(typeof process.env.MONGODB_URI);
+connectDB();
+
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     console.log("Connected to the database");
+//     app.on("error", (error) => {
+//       console.error("Error: ", error);
+//     });
+//
+//     app.listen(process.env.PORT, () => {
+//       console.log(`Server is running on port ${process.env.PORT}`);
+//     });
+//
+//   } catch (error) {
+//     console.error("Error: ", error);
+//   }
+// })();
